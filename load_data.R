93a69f7d2160706c8d24f46b12e8d7df5a569ede
karanmurthy7 <karanmurthy7.applications@gmail.com>
2017-02-26 18:40:57 -0800

    Update load_data.R

--- load_data.R
+++ load_data.R
@@ -98,8 +98,8 @@ min.max.offense2200 <- cleaned_crime_data %>%
   filter(Summary.Offense.Code == 2200) %>%
   summarise(count_minmax = n()) %>%
   arrange(desc(count_minmax))
-
 mean(min.max.offense2200$count_minmax)
+
 # In 2016, 864.5 burglaries occurred in Seattle per month. 
 sd(min.max.offense2200$count_minmax)
 
@@ -117,20 +117,35 @@ sd(min.max.offense2400$count_minmax)
 
 crime.data.2300 <- cleaned_crime_data %>%
   filter(Summary.Offense.Code == 2300)
-hist(crime.data.2300$Month, xlab = "Months", main = "Histogram of Crime - Theft (Car prowl, shoplift, building, etc.) for year 2016")
 
-plot(y = log(min.max.offense2300$count_minmax), x = min.max.offense2300$Month, xlab = "Month", ylab = "Count of Thefts", 
-     main = "Number of Thefts vs Month")
+ggplot(data=crime.data.2300, aes(crime.data.2300$Month)) + 
+  xlab("Month") + 
+  geom_histogram(stat = "count") + 
+  ggtitle("Histogram of Theft (Car prowl, shoplift, building, etc.) for the year 2016")
+
+
+ggplot(data = min.max.offense2300, aes(x = min.max.offense2300$Month,y = log(min.max.offense2300$count_minmax))) + 
+  geom_point() +
+  xlab("Month") + 
+  ylab("Log of number of Thefts") + 
+  ggtitle("Log of number of Thefts vs Month") + 
+  geom_smooth(method = "lm")
+
 mod.2300.month <- lm(formula = log(min.max.offense2300$count_minmax) ~ min.max.offense2300$Month, data = min.max.offense2300)
+
 summary(mod.2300.month)
 # p-value = 4.67e-07 implies statistical significance
 
 crime.data.2200 <- cleaned_crime_data %>%
   filter(Summary.Offense.Code == 2200)
-hist(crime.data.2200$Month, xlab = "Months", main = "Histogram of Crime - Burglary for year 2016")
 
-plot(y = log(min.max.offense2200$count_minmax), x = min.max.offense2200$Month, xlab = "Month", ylab = "Count of Burglaries", 
-     main = "Number of Burglaries vs Month")
+ggplot(data=crime.data.2200, aes(crime.data.2200$Month)) + 
+  xlab("Month") + 
+  geom_histogram(stat = "count") + 
+  ggtitle("Histogram of Burglary for the year 2016")
+
+ggplot(data = min.max.offense2200, aes(x = min.max.offense2200$Month,y = log(min.max.offense2200$count_minmax))) + geom_point() +
+  xlab("Month") + ylab("Log of number of Burglaries") + ggtitle("Log of number of Burglaries vs Month") + geom_smooth(method = "lm")
 mod.2200.month <- lm(formula = log(min.max.offense2200$count_minmax) ~ min.max.offense2200$Month, data = min.max.offense2200)
 summary(mod.2200.month)
 # p-value = 1.72e-07 implies statistical significance
@@ -138,10 +153,19 @@ summary(mod.2200.month)
 
 crime.data.2400 <- cleaned_crime_data %>%
   filter(Summary.Offense.Code == 2400)
-hist(crime.data.2400$Month, xlab = "Months", main = "Histogram of Crime - Vehicle Theft for year 2016")
 
-plot(y = log(min.max.offense2400$count_minmax), x = min.max.offense2400$Month, xlab = "Month", ylab = "Count of Vehicle Thefts", 
-     main = "Number of Vehicle Thefts vs Month")
+ggplot(data=crime.data.2400, aes(crime.data.2400$Month)) + 
+  xlab("Month") + 
+  geom_histogram(stat = "count") + 
+  ggtitle("Histogram of Vehicle Theft for the year 2016")
+
+ggplot(data = min.max.offense2400, aes(x = min.max.offense2400$Month,y = log(min.max.offense2400$count_minmax))) + 
+  geom_point() +
+  xlab("Month") + 
+  ylab("Log of number of Vehicle Thefts") + 
+  ggtitle("Log of number of Vehicle Thefts vs Month") + 
+  geom_smooth(method = "lm")
+
 mod.2400.month <- lm(formula = log(min.max.offense2400$count_minmax) ~ min.max.offense2400$Month, data = min.max.offense2400)
 summary(mod.2400.month)
 # p-value = 2.02e-06 implies statistical significance
@@ -149,19 +173,18 @@ summary(mod.2400.month)
 
 
 #Zone
+
 ggplot(data=cleaned_crime_data, aes(cleaned_crime_data$Zone.Beat)) + 
   geom_histogram(stat = "count") + 
   xlab("Zone") +
   ylab("Number of crimes") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   ggtitle("Zone wise distribution of crimes")
 
 mean(crime_by_zone$count)
 sd(crime_by_zone$count)
 
 time_zone_data <- read.csv("timeZones.csv")
-
-#Time of the day
 formatted_times <- format(strptime(cleaned_crime_data$Occurred.Time, "%I:%M:%S %p"), format = "%H:%M:%S")
 df <- as.data.frame(formatted_times)
 df$formatted_times_numeric <- as.numeric(as.POSIXct(formatted_times, format = "%H:%M:%S"))
@@ -179,7 +202,7 @@ time_join_df <- sqldf("select * from df
 time_join_df_clean <- time_join_df %>%
   select(formatted_times, periodId, start_time, end_time, Desc)
 
-ggplot(time_join_df_clean, aes(factor(Desc, levels = c("12 AM to 2 AM", "2 AM to 4 AM", "4 AM to 6 AM", "6 AM to 8 AM", "8 AM to 10 AM", "10 AM to 12 PM", "12 PM to 2 PM", "2 PM to 4 PM", "4 PM to 6 PM", "6 PM to 8 PM", "8 PM to 10 PM", "10 PM to midnight")))) + 
+ggplot(time_join_df_clean, aes(factor(Desc, levels = c("12 AM to 2 AM", "2 AM to 4 AM", "4 AM to 6 AM", "6 AM to 8 AM", "8 AM to 10 AM", "10 AM to 12 PM", "12 PM to 2 PM", "2 PM to 4 PM", "4 PM to 6 PM", "6 PM to 8 PM", "8 PM to 10 PM", "10 PM to midnight")))) +
     xlab("Time of the day") +
     ylab("Number of crimes") +
     ggtitle("Distribution crimes across different times of the day") +
@@ -194,10 +217,15 @@ time_wise_crime_count <- time_join_df_clean %>%
 
 mean(time_wise_crime_count$count)
 sd(time_wise_crime_count$count)
-plot(y = time_wise_crime_count$count, x= time_wise_crime_count$periodId, xlab = "Time of day",
-     ylab = "Number of criminal activities", main = "Number of criminal activities vs time of the day")
-mod.crime.time <- lm (formula = time_wise_crime_count$count ~ time_wise_crime_count$periodId, 
-                      data = time_wise_crime_count)
+
+mod.crime.time <- lm (formula = time_wise_crime_count$count ~ time_wise_crime_count$periodId, data = time_wise_crime_count)
+
+ggplot(data = time_wise_crime_count, aes(x = periodId,y = count)) + 
+  geom_point() +
+  xlab("Number of criminal activities vs time of the day") + 
+  ylab("Number of criminal activities") + 
+  ggtitle("Distribution of crime across different times of the day") + geom_smooth(method = "lm")
+
 summary(mod.crime.time)
 # Most of the criminal activities occur during the latter half of the day (12 p.m. to around 2 a.m)
 # p-value = 0.00618 implies statistical significance
